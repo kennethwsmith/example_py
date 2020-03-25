78f26aae7db0464c30475fdd341b29659f315960
Ken Smith <ksmith@ritchiebros.com>
2020-03-24 22:28:53 -0500

    test changes...

--- exampleunittest.py
+++ exampleunittest.py
@@ -11,11 +11,13 @@ class UnitTests(unittest.TestCase):
         self.assertEquals(2,example.divide(4,2))
         self.assertTrue(example.divide(40,20) == 2)
         self.assertFalse(example.divide(33,54) == 7)
-        self.assertFalse(example.divide(5,0) == 7)
+        # self.assertFalse(example.divide(5,0) == 7)
     
     def test_Arrays(self):
         a = arr.array('i', [1,2,3])
         self.assertEquals(a,example.getArray())
+        self.assertIn(2,example.getArray())
+        self.assertNotIn(4,example.getArray())
 
 if __name__ == '__main__':
     unittest.main()
