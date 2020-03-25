f94672bf63184a4e980dcbdf321f4dca8a06fb96
Ken Smith <ksmith@ritchiebros.com>
2020-03-24 22:16:52 -0500

    initial commit

---
+++ example.py
@@ -0,0 +1,11 @@
+import array as arr
+
+def add(a,b):
+    return a+b
+
+def divide(a,b):
+    return a/b
+
+def getArray():
+    a = arr.array('i',[1,2,3])
+    return a
---
+++ exampleunittest.py
@@ -0,0 +1,21 @@
+import unittest
+import example
+import array as arr
+
+class UnitTests(unittest.TestCase):
+    def test_add(self):
+        self.assertEquals(4,example.add(2,2))
+        self.assertEquals(4,example.add(2,2))
+
+    def test_divide(self):
+        self.assertEquals(2,example.divide(4,2))
+        self.assertTrue(example.divide(40,20) == 2)
+        self.assertFalse(example.divide(33,54) == 7)
+        self.assertFalse(example.divide(5,0) == 7)
+    
+    def test_Arrays(self):
+        a = arr.array('i', [1,2,3])
+        self.assertEquals(a,example.getArray())
+
+if __name__ == '__main__':
+    unittest.main()
